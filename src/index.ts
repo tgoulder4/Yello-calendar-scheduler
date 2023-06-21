d27786d744aba049b23ea8e758aeeac91b0de29b
tgoulder4 <118992509+tgoulder4@users.noreply.github.com>
2023-06-21 23:02:22 +0100

    Attempted to create a database connection using pg

--- src/index.ts
+++ src/index.ts
@@ -1,14 +1,8 @@
 import { MikroORM } from "@mikro-orm/core";
 //given that microORM.init returns a promise, set
 //microORM to the variable 'orm'.
-const main = async () => {
-  try {
-    const orm = await MikroORM.init({
-      dbName: "postgres",
-      type: "postgresql",
-    });
-  } catch (error) {
-    console.log(error);
-  }
-};
-main();
+//Corrected based on accuired knowledge so far
+const orm = await MikroORM.init({
+  dbName: "postgres",
+  type: "postgresql",
+});
