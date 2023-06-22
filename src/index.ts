c6c43053dcd8727c3c235f5a47dc26d2056f41d1
tgoulder4 <118992509+tgoulder4@users.noreply.github.com>
2023-06-22 20:56:14 +0100

    Learnt some more about decorators

--- src/index.ts
+++ src/index.ts
@@ -2,7 +2,8 @@ import { MikroORM } from "@mikro-orm/core";
 //given that microORM.init returns a promise, set
 //microORM to the variable 'orm'.
 //Corrected based on accuired knowledge so far
-const orm = await MikroORM.init({
+const orm = MikroORM.init({
   dbName: "postgres",
   type: "postgresql",
 });
+console.log(orm);
