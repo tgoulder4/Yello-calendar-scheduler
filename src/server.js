381cb60c7888ef7677e2458c209116ee90b71c42
tgoulder4 <118992509+tgoulder4@users.noreply.github.com>
2023-06-26 01:15:25 +0100

    ready for routing configuration

--- src/server.js
+++ src/server.js
@@ -1,38 +1,11 @@
 import mongoose from "mongoose";
+//schemas
 import userSchema from "./entities/user.js";
 import eventSchema from "./entities/event.js";
 import mascotSchema from "./entities/mascot.js";
+//create models
 const User = mongoose.model("User", userSchema);
 const Event = mongoose.model("Event", eventSchema);
 const Mascot = mongoose.model("Mascot", mascotSchema);
+//path to current db (dev)
 const URI = "mongodb://localhost:27017/admin";
-const main = async () => {
-  try {
-    await mongoose.connect(URI);
-    const newUser = new User({
-      displayname: "Tye",
-      username: "tyeg",
-      password: "tg",
-    });
-    const newEvent = new Event({
-      name: "Lunch with mum",
-    });
-    const mascot = new Mascot({
-      lastState: {
-        emotion: "sad",
-        reason: "ended session early",
-      },
-    });
-    try {
-      newUser.save();
-      newEvent.save();
-      mascot.save();
-      console.log(`saved!`);
-    } catch (err) {
-      console.log(`error! ${err}`);
-    }
-  } catch (err) {
-    console.error(`connection failed with error ${err}`);
-  }
-};
-main();
