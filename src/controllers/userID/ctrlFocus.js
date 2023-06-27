9215ac336d67e06a09c939f904fe118ce8c9efc5
tgoulder4 <118992509+tgoulder4@users.noreply.github.com>
2023-06-27 11:49:08 +0100

    Get routes specified

---
+++ src/controllers/userID/ctrlFocus.js
@@ -0,0 +1,4 @@
+const app = require("express")();
+module.exports = {
+  get: (req, res) => console.log("focus page"),
+};
--- src/server.js
+++ src/server.js
@@ -1,11 +1,21 @@
 const { Event, Mascot, User } = require("./setup/models");
-const ctrlSignUp = require("./routes/controllers/signUpController");
+const ctrlSignUp = require("./controllers/ctrlSignUp");
+const ctrlLogin = require("./controllers/ctrlLogin");
+const ctrlCalendar = require("./controllers/userID/ctrlCalendar");
+const ctrlCreateWeek = require("./controllers/userID/ctrlCreateWeek");
+const ctrlFocus = require("./controllers/userID/ctrlFocus");
+const ctrlHome = require("./controllers/userID/ctrlHome");
 const app = require("express")();
 const PORT = process.env.PORT || 3500;
 // const dirCurrentUser = require("./routes/userID/");
 // const dirCreateWeek = require("./routes/userID/create-week/");
 console.log(app);
-app.get("/", (req, res) => ctrlSignUp.get());
+app.get("/signup", (req, res) => ctrlSignUp.get());
+app.get("/login", (req, res) => ctrlLogin.get());
+app.get("/:userID/focus", (req, res) => ctrlFocus.get(userID));
+app.get("/:userID/createWeek", (req, res) => ctrlCreateWeek.get(userID));
+app.get("/:userID/calendar", (req, res) => ctrlCalendar.get(userID));
+app.get("/:userID/createWeek", (req, res) => ctrlHome.get(userID));
 //get: signup, login, [userid/]+[createWeek/], focus
 // app.listen(PORT, () => {
 //   console.log(`Server running on port ${PORT}`);
