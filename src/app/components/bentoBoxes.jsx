befd6f0eccb911816e625620de279a420a157e53
tgoulder4 <118992509+tgoulder4@users.noreply.github.com>
2023-07-15 19:40:18 +0100

    fixed rounding issue

--- src/app/components/bentoBoxes.jsx
+++ src/app/components/bentoBoxes.jsx
@@ -3,52 +3,61 @@ function BentoBoxes() {
   return (
     <>
       <Section
-        split={false}
-        leftContent={
+        split={true}
+        title="Now entering focus mode."
+        subtitle="Let's do more. Together."
+        layout="leftVisual"
+        visualBackground="wooden-table"
+        width={["w-2/3", "w-1/3"]}
+        content={
           <>
-            <div className="flex justify-end bg-wooden-table h-[24rem]">
-              <div className="contentContainer ">
-                <div className="paper"></div>
-                <div className="phoneFrame"></div>
-              </div>
+            <div className="flex justify-end h-[24rem]">
+              <div className="paper"></div>
+              <div className="phoneFrame"></div>
             </div>
           </>
         }
-        rightContent={
-          <div className="flex justify-start">
-            <div className="flex justify-start">
-              <h1 className="font-bold text-white text-4xl">
-                Now entering focus mode.
-              </h1>
-            </div>
-          </div>
-        }
       ></Section>
+      {/* <Section
+        split={true}
+        title="Daily Focus improves wellbeing by 70%."
+        subtitle="Manjo helps you spend at least 2 hours a day on task without distractions."
+        layout="rightVisual"
+      ></Section> */}
     </>
   );
 }
 
-function TextBox({ rounded = "none", gem, title, subtitle }) {
+function TextBox({ rounded = "rounded-none", gem, title, subtitle }) {
   return (
-    <div className={`w-full bg-[#080808] rounded-${rounded} p-[3.125rem]`}>
+    <div
+      className={`w-full bg-[#080808] flex flex-col gap-3.5 ${rounded} p-[3.125rem]`}
+    >
       {gem ? <h1 className="text-5xl">{gem}</h1> : <></>}
       <h1 className="font-bold text-white text-4xl">{title}</h1>
-      <h2>{subtitle}</h2>
+      <h2 className="text-lg text-gray-400">{subtitle}</h2>
     </div>
   );
 }
-function VisualsBox({ children, rounded = "none" }) {
+function VisualsBox({
+  children,
+  rounded = "none",
+  visualBackground = "[#080808]",
+}) {
   //visuals are the box which has the picture in it.
   return (
-    <div className={`w-full bg-[#080808] rounded-${rounded}`}>{children}</div>
+    <div className={`w-full bg-${visualBackground} ${rounded}`}>{children}</div>
   );
 }
-//I want to use this like <Box rounded="3xl"></Box>
 function Section({
-  leftContent,
-  rightContent,
+  title,
+  subtitle,
+  gem,
   split = false,
-  layout = "leftVisual",
+  content,
+  layout = "rightVisual",
+  visualBackground,
+  width = ["w-1/2", "w-1/2"],
 }) {
   //i want split to be optional and default to false
   return (
@@ -58,13 +67,35 @@ function Section({
           {
             (layout = "leftVisual" ? (
               <>
-                <VisualsBox rounded="3xl">{leftContent}</VisualsBox>
-                <TextBox rounded="3xl">{rightContent}</TextBox>
+                <VisualsBox
+                  className={`${width[0]}`}
+                  visualBackground={visualBackground}
+                  rounded="rounded-3xl"
+                >
+                  {content}
+                </VisualsBox>
+                <TextBox
+                  title={title}
+                  subtitle={subtitle}
+                  gem={gem}
+                  rounded="rounded-3xl"
+                  className={`${width[1]}`}
+                ></TextBox>
               </>
             ) : (
               <>
-                <TextBox rounded="3xl">{rightContent}</TextBox>
-                <VisualsBox rounded="3xl">{leftContent}</VisualsBox>
+                <TextBox
+                  title={title}
+                  subtitle={subtitle}
+                  gem={gem}
+                  rounded="rounded-3xl"
+                ></TextBox>
+                <VisualsBox
+                  visualBackground={visualBackground}
+                  rounded="rounded-3xl"
+                >
+                  {content}
+                </VisualsBox>
               </>
             ))
           }
@@ -72,13 +103,34 @@ function Section({
       ) : (
         (layout = "leftVisual" ? (
           <section className="flex">
-            <VisualsBox rounded="l-3xl" gem="none"></VisualsBox>
-            <TextBox rounded="r-3xl"></TextBox>
+            <VisualsBox
+              visualBackground={visualBackground}
+              rounded="rounded-l-3xl"
+              gem="none"
+            >
+              {content}
+            </VisualsBox>
+            <TextBox
+              title={title}
+              subtitle={subtitle}
+              gem={gem}
+              rounded="rounded-r-3xl"
+            ></TextBox>
           </section>
         ) : (
           <section className="flex">
-            <TextBox rounded="l-3xl">{rightContent}</TextBox>
-            <VisualsBox rounded="r-3xl">{leftContent}</VisualsBox>
+            <TextBox
+              title={title}
+              subtitle={subtitle}
+              gem={gem}
+              rounded="rounded-l-3xl"
+            ></TextBox>
+            <VisualsBox
+              visualBackground={visualBackground}
+              rounded="rounded-r-3xl"
+            >
+              {content}
+            </VisualsBox>
           </section>
         ))
       )}
