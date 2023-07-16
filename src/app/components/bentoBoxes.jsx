d48ee5830777658f698402204bfd777b81924223
tgoulder4 <118992509+tgoulder4@users.noreply.github.com>
2023-07-16 03:55:51 +0100

    refactored Section

--- src/app/components/bentoBoxes.jsx
+++ src/app/components/bentoBoxes.jsx
@@ -5,19 +5,23 @@
 //visualLocation => The location of the visual box wrt the text box. E.g. =right => TextBox | VisualBox
 //width = ["w-1/2","w-1/2"] => The boxes will fill an equal amount of space
 //content => The content of the content box.
+
+//Notes for next:
+//I want the phone to stay in the middle.
+//singleBoxes need to be fixed.
 function BentoBoxes() {
   return (
     <>
       <Section
-        split={true}
+        split={false}
+        orderSwap={true}
         title="Now entering focus mode."
         subtitle="Let's do more. Together."
-        visualLocation="left"
-        visualBackground="wooden-table"
-        width={["w-2/3", "w-1/3"]}
+        visualBackground="bg-wooden-table"
+        widths={["w-2/3", "w-1/3"]}
         content={
           <>
-            <div className="flex justify-end h-[24rem]">
+            <div className="flex justify-end h-[21rem]">
               <img
                 src="/assets/Paper.png"
                 className="object-fit"
@@ -38,33 +42,44 @@ function BentoBoxes() {
         split={true}
         title="Daily Focus improves wellbeing by 70%."
         subtitle="Manjo helps you spend at least 2 hours a day on task without distractions."
-        visualLocation="right"
+        widths={["w-3/5", "w-2/5"]}
       ></Section>
-      <Section split={false} mode="singleBox" title="singlebox test"></Section>
+      <Section
+        split={false}
+        oneBox={true}
+        title="The new you."
+        subtitle="Whether you’re studying French, practicing piano or simply having quiet time, Manjo can help you achieve peace of mind."
+      ></Section>
     </>
   );
 }
 
-function TextBox({ rounded = "rounded-3xl", gem, title, subtitle, width }) {
+function TextBox({
+  borderRadius = "rounded-3xl",
+  gem,
+  title,
+  subtitle,
+  width,
+}) {
   return (
     <div
-      className={`w-full bg-[#080808] flex flex-col gap-3.5 ${width} ${rounded} p-[3.125rem]`}
+      className={`${width} bg-[#080808] flex flex-col gap-3.5 ${borderRadius} p-[2.375rem]`}
     >
       {gem ? <h1 className="text-5xl">{gem}</h1> : <></>}
-      <h1 className="font-bold text-white text-4xl">{title}</h1>
+      <h1 className="font-bold text-white text-3xl">{title}</h1>
       <h2 className="text-lg text-gray-400">{subtitle}</h2>
     </div>
   );
 }
 function VisualsBox({
   children,
-  rounded = "rounded-3xl",
-  visualBackground = "[#080808]",
+  borderRadius = "rounded-3xl",
+  visualBackground = "bg-[#080808]",
   width,
 }) {
   //visuals are the box which has the picture in it.
   return (
-    <div className={`w-full bg-${visualBackground} ${width} ${rounded}`}>
+    <div className={`w-full ${visualBackground} ${width} ${borderRadius}`}>
       {children}
     </div>
   );
@@ -73,11 +88,13 @@ function SingleBox({
   gem,
   title,
   subtitle,
-  rounded = "rounded-3xl",
+  borderRadius = "rounded-3xl",
   content = "",
 }) {
   return (
-    <div className={`flex justify-center items-center ${rounded}`}>
+    <div
+      className={`flex flex-col bg-[#080808] w-full p-[2.375rem] justify-center items-center ${borderRadius}`}
+    >
       {/* content or this below */}
       {content}
       {gem ? <h1 className="text-5xl">{gem}</h1> : <></>}
@@ -86,118 +103,63 @@ function SingleBox({
     </div>
   );
 }
-
 function Section({
   title,
   subtitle,
   gem,
   split = false,
   content = "",
-  visualLocation = "right",
   visualBackground,
-  width = ["w-1/2", "w-1/2"],
-  mode = "twoBoxes",
+  widths = ["w-1/2", "w-1/2"],
+  borderRadius = ["rounded-l-3xl", "rounded-r-3xl"],
+  orderSwap = false,
+  oneBox = false,
 }) {
-  //i want split to be optional and default to false
+  //so the correct props are generated BEFORE the elements are created with them
+  if (orderSwap) {
+    borderRadius.sort((a, b) => -1);
+    widths.sort((a, b) => -1);
+  }
+  if (split) {
+    borderRadius.fill("rounded-3xl");
+    //twoBoxes[0].addProp("rounded-l-3xl")
+  }
+  let twoBoxes = [
+    <TextBox
+      title={title}
+      subtitle={subtitle}
+      gem={gem}
+      borderRadius={borderRadius[0]}
+      width={widths[0]}
+    ></TextBox>,
+    <VisualsBox
+      visualBackground={visualBackground}
+      borderRadius={borderRadius[1]}
+      width={widths[1]}
+    >
+      {content}
+    </VisualsBox>,
+  ];
+  let gap = "";
+  //what about widths,border radii after sorting? how do I do that
+  if (orderSwap) {
+    twoBoxes.sort((a, b) => -1);
+    //if the boxes have just been sorted WITH THE RADII THEN WHY ARE THEY THE WRONG WAY
+  }
+  if (split) {
+    gap = "gap-8";
+    //twoBoxes[0].addProp("rounded-l-3xl")
+  }
   return (
     <>
-      {/* an absolute mess. needs refactoring:
-      content,title,subtitle,gem is by default "".
-        function Section({
-
-        }){
-            return (
-                one box?
-	                <SingleBox title={title}... >{Content}</SingleBox>
-                :
-                {visualLocation == left?
-	                twoBoxes.swap(a,b=>-1)
-	                widths.swap(as above)
-                    split? borderRadius=3xl : []
-                }
-                <section>{twoBoxes}</section>
-const twoBoxes = [
-<TextBox title={title} subtitle={subtitle}...> </TextBox>
-<VisualBox>{Content}</VisualBox>]
-        }
-
-      */}
-      {mode == "twoBoxes" ? (
-        split ? (
-          <section className="flex gap-5">
-            {visualLocation == "left" ? (
-              <>
-                <VisualsBox
-                  width={`${width[0]}`}
-                  visualBackground={visualBackground}
-                >
-                  {content}
-                </VisualsBox>
-                <TextBox
-                  title={title}
-                  subtitle={subtitle}
-                  gem={gem}
-                  width={`${width[1]}`}
-                ></TextBox>
-              </>
-            ) : (
-              <>
-                <TextBox
-                  title={title}
-                  subtitle={subtitle}
-                  gem={gem}
-                  width={`${width[0]}`}
-                ></TextBox>
-                <VisualsBox
-                  visualBackground={visualBackground}
-                  width={`${width[1]}`}
-                >
-                  {content}
-                </VisualsBox>
-              </>
-            )}
-          </section>
-        ) : visualLocation == "left" ? (
-          <section className="flex">
-            <VisualsBox
-              visualBackground={visualBackground}
-              rounded="rounded-l-3xl"
-              gem="none"
-              width={`${width[0]}`}
-            >
-              {content}
-            </VisualsBox>
-            <TextBox
-              title={title}
-              subtitle={subtitle}
-              gem={gem}
-              rounded="rounded-r-3xl"
-              width={`${width[1]}`}
-            ></TextBox>
-          </section>
-        ) : (
-          <section className="flex">
-            <TextBox
-              title={title}
-              subtitle={subtitle}
-              gem={gem}
-              rounded="rounded-l-3xl"
-              width={`${width[0]}`}
-            ></TextBox>
-            <VisualsBox
-              visualBackground={visualBackground}
-              rounded="rounded-r-3xl"
-              width={`${width[1]}`}
-            >
-              {content}
-            </VisualsBox>
-          </section>
-        )
-      ) : (
-        <section>
+      <section className={`flex ${gap}`}>
+        {oneBox ? (
           <SingleBox gem={gem} title={title} subtitle={subtitle}></SingleBox>
-        </section>
-      )}
+        ) : (
+          //there are multiple boxes here
+          twoBoxes
+        )}
+      </section>
     </>
   );
 }
--- src/app/layout.jsx
+++ src/app/layout.jsx
@@ -11,15 +11,15 @@ export const metadata = {
 export default function RootLayout({ children }) {
   return (
     <html lang="en">
-      <body className="${inter.className} px-[240px] py-[50px] mx-auto min-h-[100vh] bg-[color:var(--primaryLight)]">
+      <body className="${inter.className} px-[180px] py-[30px] mx-auto min-h-[100vh] bg-[color:var(--primaryLight)]">
         {/*nav!!!*/}
         <main className="outline outline-offset-2 outline-pink-500 flex flex-col gap-8">
-          <nav className="flex justify-between h-8">
+          <nav className="flex justify-between h-[1.5rem]">
             <img
               src="/assets/manjo.png"
               draggable="false"
               alt="Manjo"
-              className="object-fill h-fit"
+              className="object-fill h-[inherit]"
             />
             <FontAwesomeIcon
               className="max-w-[30px]"
